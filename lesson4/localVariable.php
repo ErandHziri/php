1987f6395f0c93cd2e7354d0c8a45d99bc6303bc
ErandHaziri <erand.haziri1@gmail.com>
2025-05-14 12:10:43 +0200

    lesson 4

---
+++ lesson4/localVariable.php
@@ -0,0 +1,17 @@
+<?php
+    $x=5; //global varible
+    
+    function localVariable(){
+        $y=10; //local variable
+        //echo $x;
+        echo $y;
+    }
+
+    localVariable();
+    echo  "\n,  $x";
+
+?>
+
+
+
+
